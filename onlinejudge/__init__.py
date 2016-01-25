692044d4d51416bdc5e33ded4e91db7cc71c5d1e
Krishak Choukhany <krishakchoukhany@gmail.com>
2016-01-25 23:47:12 +0530

    initial commit

---
+++ onlinejudge/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask,render_template,request
+from flask.ext.script import Manager
+from subprocess import Popen, PIPE
+
+app = Flask(__name__)
+app.debug = True
+manager = Manager(app)
+classname='hello'
+lang = {"Python":('ex.py','python ex.py'),"C":('hello.c','gcc hello.c;./a.out'),"C++":('hello.cpp','g++ hello.cpp;./a.out'),"Java":(classname+'.java','javac '+classname+'.java;java '+classname)}
+
+
+@app.route("/",methods=['GET', 'POST'])
+def hello():
+
+	if request.method == 'GET':
+		return render_template("home.html",output_value = "")
+		
+	if request.method == 'POST':
+		code = request.form.get('code')
+		language = request.form.get('lang')
+		if language == 'Java':
+			#Get class name of java file
+			pass
+		l = lang[language]
+		with open(l[0],'w') as f:
+			f.write(code)
+		terminal_processes = Popen(l[1],shell=True,stdout=PIPE)
+		output = terminal_processes.communicate()[0]
+		return render_template("home.html",output_value=output)
+    	
+
