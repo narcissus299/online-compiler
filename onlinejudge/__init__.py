71b686303f5d5c5ff4016b7f0d509633b04d43f3
Krishak Choukhany <krishakchoukhany@gmail.com>
2016-01-28 21:29:57 +0530

    Added input option

--- onlinejudge/__init__.py
+++ onlinejudge/__init__.py
@@ -6,17 +6,20 @@ app = Flask(__name__)
 app.debug = True
 manager = Manager(app)
 classname='hello'
-lang = {"Python":('ex.py','python ex.py'),"C":('hello.c','gcc hello.c;./a.out'),"C++":('hello.cpp','g++ hello.cpp;./a.out'),"Java":(classname+'.java','javac '+classname+'.java;java '+classname)}
+lang = {"Python":('ex.py','python ex.py'),"C":('hello.c','gcc hello.c;./a.out'),"Python3":('ex.py','python3 ex.py'),"C++":('hello.cpp','g++ hello.cpp;./a.out'),"Java":(classname+'.java','javac '+classname+'.java;java '+classname)}
 
 
 @app.route("/",methods=['GET', 'POST'])
 def index():
 
 	if request.method == 'GET':
-		return render_template("home.html",output_value = "")
+		return render_template("home.html",output_value = "",code_text="")
 		
 	if request.method == 'POST':
+		inp=""
+		code=""
 		code = request.form.get('code')
+		inp = request.form.get('input')
 		language = request.form.get('lang')
 		if language == 'Java':
 			#Get class name of java file
@@ -24,7 +27,7 @@ def index():
 		l = lang[language]
 		with open(l[0],'w') as f:
 			f.write(code)
-		terminal_processes = Popen(l[1],shell=True,stdout=PIPE)
-		output = terminal_processes.communicate()[0]
-		return render_template("home.html",output_value=output)
+		terminal_processes = Popen(l[1],shell=True,stdin=PIPE,stdout=PIPE)
+		output = terminal_processes.communicate(inp)[0]
+		return render_template("home.html",output_value=output,code_text=code)
     
